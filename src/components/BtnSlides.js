8f9b85c32fca081ecd6121b2d1e5040c8d9fbc35
Damien Monasterios <monasterio13septiembre@gmail.com>
2023-04-27 19:20:34 -0500

    Ajustes minimos

--- src/components/BtnSlides.js
+++ src/components/BtnSlides.js
@@ -5,7 +5,7 @@ const BtnSlides = ({ page, setPage, maxPage }) => {
 
     return (
         <div id="navigation" className="text-center">
-            <button  data-testid="button-restart" onClick={(x) => SliderAction(page, setPage, maxPage, x)} className={"small outlined"}>Reiniciar</button>
+            <button data-testid="button-restart" onClick={(x) => SliderAction(page, setPage, maxPage, x)} className={page === 0 ? 'hidden' : "small outlined"}>Reiniciar</button>
             <button data-testid="button-prev" onClick={(x) => SliderAction(page, setPage, maxPage, x)} className={page === 0 ? 'small disabled' : "small"}>Ant.</button>
             <button data-testid="button-next" onClick={(x) => SliderAction(page, setPage, maxPage, x)} className={maxPage === page ? 'small disabled' : "small"}>Sig.</button>
         </div>
